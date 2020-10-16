811f7e183a01121807edf0af1e8c0d5521b5cda1
svg94 <liga-2000@hotmail.de>
2020-10-16 21:41:04 +0200

    new test.py for multiple motors

--- test.py
+++ test.py
@@ -5,40 +5,37 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-A = 7
-B = 11
-C = 13
-D = 15
+A = [7, 19]			#Each Stepper Motor has 4 Wireslots  
+B = [11,21]			# represented by the lists A,B,C,D
+C = [13, 23]		# Slot 1 is A, Slot 2 is B... 
+D = [15, 29]		#4 Motors, so 4 elements in each list. (soon)
+pins= [A,B,C,D]
 
-GPIO.setup(A, GPIO.OUT)
-GPIO.setup(B,GPIO.OUT)
-GPIO.setup(C,GPIO.OUT)
-GPIO.setup(D,GPIO.OUT)
+for pinblock in pins:		#Setup all the pins
+	for pin in pinblock:
+		GPIO.setup(pin, GPIO.OUT)
 
-def GPIO_SETUP(a,b,c,d):
-    GPIO.output(A,a)
-    GPIO.output(B,b)
-    GPIO.output(C,c)
-    GPIO.output(D,d)
+def GPIO_SETUP(a,b,c,d,motor):	#which Motor 0-3
+    GPIO.output(A[motor],a)
+    GPIO.output(B[motor],b)
+    GPIO.output(C[motor],c)
+    GPIO.output(D[motor],d)
     time.sleep(0.002)
-GPIO_SETUP(0,0,0,0)
 
-for i in range(2000):
-    GPIO_SETUP(1,1,0,0)
-    GPIO_SETUP(0,1,1,0)
-    GPIO_SETUP(0,0,1,1)
-    GPIO_SETUP(1,0,0,1)
-#for i in range(2000):
- #   GPIO_SETUP(1,0,0,0)
-  #  GPIO_SETUP(0,1,0,0)
-   # GPIO_SETUP(0,0,1,0)
-   # GPIO_SETUP(0,0,0,1)
+for i in range(len(A)):#Taking A, assuming every list has equal length
+		GPIO_SETUP(0,0,0,0,i)
+	
 
-
-
-
-
-
-GPIO_SETUP(0,0,0,0)
-
+for i in range(1000):
+    GPIO_SETUP(1,1,0,0,1)
+    GPIO_SETUP(0,1,1,0,1)
+    GPIO_SETUP(0,0,1,1,1)
+    GPIO_SETUP(1,0,0,1,1)
+#for i in range(2000):      #Wave Rotation. Very Basic not so precise or fast.
+#   GPIO_SETUP(1,0,0,0)
+#  GPIO_SETUP(0,1,0,0)
+# GPIO_SETUP(0,0,1,0)
+# GPIO_SETUP(0,0,0,1)
+for i in range(len(A)):
+	GPIO_SETUP(0,0,0,0,i)
 
