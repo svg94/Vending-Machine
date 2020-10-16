7057ac44d398c1e7f622d00e8799b3c591403834
svg94 <liga-2000@hotmail.de>
2020-10-16 17:37:25 +0200

    new structure. Motor rotates after putting succ in quary

--- backend/vending.py
+++ backend/vending.py
@@ -1,6 +1,6 @@
 import time
 import RPi.GPIO as GPIO
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 GPIO.setmode(GPIO.BOARD)
@@ -16,22 +16,28 @@ GPIO.setup(C,GPIO.OUT)
 GPIO.setup(D,GPIO.OUT)
 
 def GPIO_SETUP(a,b,c,d):
-    GPIO.output(A,a)
-    GPIO.output(B,b)
-    GPIO.output(C,c)
-    GPIO.output(D,d)
-    time.sleep(0.002)
+	GPIO.output(A,a)
+	GPIO.output(B,b)
+	GPIO.output(C,c)
+	GPIO.output(D,d)
+	time.sleep(0.002)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    GPIO_SETUP(0,0,0,0)
-    for i in range(2000):
-        GPIO_SETUP(1,1,0,0)
-        GPIO_SETUP(0,1,1,0)
-        GPIO_SETUP(0,0,1,1)
-        GPIO_SETUP(1,0,0,1)
-    GPIO_SETUP(0,0,0,0)
-    return jsonify({"message": "Here your snack."})
+	status = request.args.get('status')	
+	text=""
+	if status == 'succ':				
+		GPIO_SETUP(0,0,0,0)
+		for i in range(500):
+			for i in range(2):		#One Motor cicle is 8 steps.    
+				GPIO_SETUP(1,1,0,0)
+				GPIO_SETUP(0,1,1,0)
+				GPIO_SETUP(0,0,1,1)
+				GPIO_SETUP(1,0,0,1)
+		text="Snack retrieved. Don't get any more, bro. It's for your own health."
+		return render_template("index.html",name=text
+	GPIO_SETUP(0,0,0,0)
+	return render_template("index.html",name =text)	
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
