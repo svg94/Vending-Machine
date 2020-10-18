3fa3f08815b8f4e1e70fa5a13162419061a154ad
Bojan Savic <liga-2000@hotmail.de>
2020-10-18 11:24:35 +0200

    refactored vending.py

--- backend/vending.py
+++ backend/vending.py
@@ -1,44 +1,21 @@
-import time
-import RPi.GPIO as GPIO
 from flask import Flask, request, render_template
 import motor
 
 app = Flask(__name__)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-A = 7   #TO-DO: Put PinNumbers in list
-B = 11
-C = 13
-D = 15
-
-GPIO.setup(A, GPIO.OUT)
-GPIO.setup(B,GPIO.OUT)
-GPIO.setup(C,GPIO.OUT)
-GPIO.setup(D,GPIO.OUT)
-
-def GPIO_SETUP(a,b,c,d):
-	GPIO.output(A,a)
-	GPIO.output(B,b)
-	GPIO.output(C,c)
-	GPIO.output(D,d)
-	time.sleep(0.002)
+m1 = Motor(7,11,13,15)
+m2 = Motor(19,21,23,29)
+m3 = Motor(8,10,12,16)
+m4 = Motor(18,22,24,26)
 
 @app.route('/', methods=['GET'])
 def index():
-	status = request.args.get('status')	
+	status = request.args.get('status')
 	text=""
-	if status == 'succ':				
-		GPIO_SETUP(0,0,0,0)
-		for i in range(500):
-			for i in range(2):		#One Motor cicle is 8 steps.    
-				GPIO_SETUP(1,1,0,0)
-				GPIO_SETUP(0,1,1,0)
-				GPIO_SETUP(0,0,1,1)
-				GPIO_SETUP(1,0,0,1)
+	if status == 'succ1':
+		m1.Turn_Right(500)
 		text="Snack retrieved. Don't get any more, bro. It's for your own health."
 		return render_template("index.html",name=text
-	GPIO_SETUP(0,0,0,0)
-	return render_template("index.html",name =text)	
+	return render_template("index.html",name =text)
 
 if __name__ == "__main__":
 	app.run()
