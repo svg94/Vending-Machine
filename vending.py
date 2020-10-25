42cd1918b83b3296b0dcec5af25336df0d90665c
Bojan Savic <liga-2000@hotmail.de>
2020-10-25 17:12:43 +0100

    added insults to vending

--- vending.py
+++ vending.py
@@ -1,33 +1,31 @@
 from flask import Flask, request, render_template
 from motor import Motor
+from insult import Insults
 
 app = Flask(__name__)
 m1 = Motor(7,11,13,15)
 m2 = Motor(19,21,23,29)
 m3 = Motor(8,10,12,16)
 m4 = Motor(18,22,24,26)
+i = Insults()
 
 @app.route('/', methods=['GET'])
 def index():
 	status = request.args.get('status')
-	text=""
+	text=i.getRdmInsult()
 	if status == 'succ1':
 		m1.Turn_Right(500)
-		text="Snack retrieved. Don't get any more, bro. It's for your own health."
 		return render_template("index.html",name=text)
 	if status == 'succ2':
 		m2.Turn_Right(500)
-		text="Snack retrieved. Don't get any more, bro. It's for your own health."
 		return render_template("index.html",name=text)
 	if status == 'succ3':
 		m3.Turn_Right(500)
-		text="Snack retrieved. Don't get any more, bro. It's for your own health."
 		return render_template("index.html",name=text)
 	if status == 'succ4':
 		m4.Turn_Right(500)
-		text="Snack retrieved. Don't get any more, bro. It's for your own health."
 		return render_template("index.html",name=text)
-	return render_template("index.html",name =text)
+	return render_template("index.html", name="")
 @app.route('/settings.html', methods=['GET'])
 def settings():
     status = request.args.get('status')
