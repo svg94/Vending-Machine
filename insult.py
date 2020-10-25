a511d91cb4a57ecd63045d88942103717f825e3d
svg94 <liga-2000@hotmail.de>
2020-10-25 17:27:42 +0100

    implemented insults. Css part missing.

--- insult.py
+++ insult.py
@@ -1,7 +1,9 @@
 import random as r
+import os
+
 class Insults:
     def __init__(self):
-        file = open("static/text/insults.txt")
+        file = open(os.path.abspath(os.path.dirname(__file__)) + '/static/text/insults.txt')
         self.insults = file.read().split('\n')
     def getRdmInsult(self):
         rdmNo = r.randint(0, len(self.insults)-2)
--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,5 @@ function spin(num){
   window.open("/?status=succ"+num.toString(),"_self");
 }
 function refill(num){
-  window.open("/?status=bacc"+num.toString(),"_self");
+  window.open("/settings.html?status=bacc"+num.toString(),"_self");
 }
